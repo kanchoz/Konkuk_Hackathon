361a923728a9698903fcc6332b3acf12c5ab72e7
taipaise <taipaise@gmail.com>
2023-08-09 22:00:32 +0900

    .

---
+++ FrontEnd/PopUp/PopUp/HomeView/View/VisitedPieceCVCell.swift
@@ -0,0 +1,13 @@
+//
+//  VisitedPieceCVCell.swift
+//  PopUp
+//
+//  Created by 이동현 on 2023/08/09.
+//
+
+import UIKit
+// MARK: - 내가 모은 방문 조각 collectionView Cell
+class VisitedPieceCVCell: UICollectionViewCell {
+    
+    @IBOutlet var visitedImg: UIImageView!
+}
