15e5e5fecd46fdcec44acc2ddacca5c02ba662ec
taipaise <taipaise@gmail.com>
2023-08-14 22:26:23 +0900

    [feat] 마이페이지, 방문한 팝업, 리뷰 작성UI구현

---
+++ FrontEnd/PopUp/PopUp/HomeView/View/MyPage/StoredTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  StoredTableViewCell.swift
+//  PopUp
+//
+//  Created by 이동현 on 2023/08/14.
+//
+
+import UIKit
+
+class StoredTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
