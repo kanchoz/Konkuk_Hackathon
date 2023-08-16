26c3c71ded67b7fcc4d2f56f3dd6803bb404f1c1
taipaise <taipaise@gmail.com>
2023-08-17 03:45:36 +0900

    지도 화면 수정

--- FrontEnd/PopUp/PopUp/HomeView/Controller/Search/StoreInfoViewController.swift
+++ FrontEnd/PopUp/PopUp/HomeView/Controller/Search/StoreInfoViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class StoreInfoViewController: UIViewController {
 
+    @IBOutlet var name: UILabel!
+    
+    @IBOutlet var locationLabel: UILabel!
+    
+    @IBOutlet var img1: UIImageView!
+    
+    @IBOutlet var img2: UIImageView!
+    
+    @IBOutlet var img3: UIImageView!
+    
+    
+    var data:Store?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        name.text = data!.name
+        locationLabel.text = data!.location
+        img1.image = UIImage(named: "\(data!.id)SearchCVImg1")
+        img2.image = UIImage(named: "\(data!.id)SearchCVImg2")
+        img3.image = UIImage(named: "\(data!.id)SearchCVImg3")
+    }
+    
+    @IBAction func detailBtnTapped(_ sender: Any) {
+        
+        let sb = UIStoryboard(name: "PieceInfoViewController", bundle: nil)
+        let nextVC = sb.instantiateViewController(withIdentifier: "PieceInfoViewController") as! PieceInfoViewController
+        
+        nextVC.modalPresentationStyle = .fullScreen
+        present(nextVC, animated: true)
+    }
+    
 }
--- FrontEnd/PopUp/PopUp/HomeView/View/Search/DummyStore.swift
+++ FrontEnd/PopUp/PopUp/HomeView/View/Search/DummyStore.swift
@@ -10,14 +10,10 @@ import MapKit
 
 class DummyStore{
    static let storeLocation = [
-            Store(name: "젠틀몬스터 이즈백", sort: "Food", coordinate: CLLocationCoordinate2D(latitude: 37.546796, longitude: 127.041957)),
-            Store(name: "파스타 믹스", sort: "Food", coordinate: CLLocationCoordinate2D(latitude: 37.546796, longitude: 127.041674)),
-            Store(name: "진로 날아오르다", sort: "Liquor", coordinate: CLLocationCoordinate2D(latitude: 37.546871, longitude: 127.042391)),
-            Store(name: "BTS", sort: "Star", coordinate: CLLocationCoordinate2D(latitude: 37.547035, longitude: 127.041136)),
-            Store(name: "르세라핌", sort: "Star", coordinate: CLLocationCoordinate2D(latitude: 37.547678, longitude: 127.041902)),
-            Store(name: "박재범의 원소주", sort: "Liquor", coordinate: CLLocationCoordinate2D(latitude: 37.546617, longitude: 127.042071)),
-            Store(name: "뉴진스 바니랜드", sort: "Star", coordinate: CLLocationCoordinate2D(latitude: 37.546829, longitude: 127.043398))
-    
+    Store(name: "젠틀몬스터 이즈백", sort: "Food", id: 6, location: "서울시 강남구", coordinate: CLLocationCoordinate2D(latitude: 37.546796, longitude: 127.041957)),
+    Store(name: "르세라핌", sort: "Star", id: 1, location: "서울시 강남구", coordinate: CLLocationCoordinate2D(latitude: 37.546796, longitude: 127.041674)),
+    Store(name: "뉴진스 바니랜드", sort: "Star", id: 9, location: "서울시 강남구", coordinate: CLLocationCoordinate2D(latitude: 37.546829, longitude: 127.043398)),
+    Store(name: "포도도", sort: "Food", id: 31, location: "서울시 강서구", coordinate: CLLocationCoordinate2D(latitude: 37.547035, longitude: 127.041136)),
     ]
 }
 
@@ -36,5 +32,7 @@ class DummyReview{
 struct Store{
     let name:String
     let sort:String
+    let id: Int
+    let location: String
     let coordinate: CLLocationCoordinate2D
 }
