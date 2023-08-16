8b15dd7bdfeb9a1fd650f48fd6565948df94c798
Jeongdonghun <arrrr180@konkuk.ac.kr>
2023-08-17 02:03:18 +0900

    [feat] home

---
+++ src/main/java/com/example/popup/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.example.popup.controller;
+
+import com.example.popup.dto.ReqDto.PopupReqDto;
+import com.example.popup.dto.ResDto.GlobalResDto;
+import com.example.popup.service.HomeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    private final HomeService homeService;
+
+
+    @GetMapping("/hotPopup")
+    public GlobalResDto<?> getHotPopup(){
+
+        return homeService.getHotPopup();
+    }
+
+    @GetMapping("/allPopup")
+    public GlobalResDto<?> getAllPopup() throws IOException {
+
+        return homeService.getAllPopup();
+    }
+}
---
+++ src/main/java/com/example/popup/service/HomeService.java
@@ -0,0 +1,44 @@
+package com.example.popup.service;
+
+import com.example.popup.domain.Popup;
+import com.example.popup.dto.ResDto.GlobalResDto;
+import com.example.popup.dto.ResDto.PopupHomeResDto;
+import com.example.popup.repository.PopupRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class HomeService {
+
+    private final PopupRepository popupRepository;
+
+    @Transactional
+    public GlobalResDto<?> getHotPopup() {
+
+        List<Popup> popups = popupRepository.findAll();
+        List<String> popupNames = popups.stream()
+                .map(Popup::getName)
+                .collect(Collectors.toList());
+
+        return GlobalResDto.success(popupNames,null);
+    }
+
+    @Transactional
+    public GlobalResDto<?> getAllPopup() throws IOException {
+
+        List<Popup> popups = popupRepository.findAll();
+
+
+        List<PopupHomeResDto> popupResDtoList = popups.stream()
+                .map(PopupHomeResDto::new)
+                .collect(Collectors.toList());
+
+        return GlobalResDto.success(popupResDtoList,null);
+    }
+}
