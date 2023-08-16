163e551c73676d40e9f3f22caca80cab66b46c6c
taipaise <taipaise@gmail.com>
2023-08-16 17:47:06 +0900

    .

---
+++ FrontEnd/PopUp/PopUp/HomeView/Controller/Main/VerifyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  VerifyViewController.swift
+//  PopUp
+//
+//  Created by 이동현 on 2023/08/16.
+//
+
+import UIKit
+
+class VerifyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
